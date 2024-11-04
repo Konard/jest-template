182d00d4ee7f0e560de8f216616aaea5cf1a4125
Konstantin Diachenko <drakonard@gmail.com>
2024-11-05 01:14:33 +0530

    More cases Make better comments and documentation

--- esm/tests/sample.test.cjs
+++ esm/tests/sample.test.cjs
@@ -1,18 +1,32 @@
 // CommonJS style test
 const { sum } = require('../src/sample.cjs');
 
-test('adds 1 + 2 to equal 3 (CommonJS)', () => {
+// CommonJS style test using static import for .mjs
+test('adds 1 + 2 to equal 3 (static require of cjs)', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-// ESM style test using dynamic import
-test('adds 1 + 2 to equal 3 (ESM)', async () => {
+// ESM style test using dynamic import for mjs
+test('adds 1 + 2 to equal 3 (dynamic import of mjs)', async () => {
   const { sum: esmSum } = await import('../src/sample.mjs');
   expect(esmSum(1, 2)).toBe(3);
 });
 
-// CommonJS style test using dynamic require
-test('adds 3 + 4 to equal 7 (dynamic require)', () => {
+// ESM style test using dynamic import for cjs
+test('adds 1 + 2 to equal 3 (dynamic import of cjs)', async () => {
+  const { sum: esmSum } = await import('../src/sample.cjs');
+  expect(esmSum(1, 2)).toBe(3);
+});
+
+// CommonJS style test using dynamic require for cjs
+test('adds 3 + 4 to equal 7 (dynamic require of cjs)', () => {
   const { sum: dynSum } = require('../src/sample.cjs');
   expect(dynSum(3, 4)).toBe(7);
+});
+
+// CommonJS style test using dynamic require for mjs (expected to always throw)
+test('adds 3 + 4 to equal 7 (dynamic require of mjs)', () => {
+  expect(() => {
+    require('../src/sample.mjs');
+  }).toThrow(/must use import to load es module/i);
 });
--- esm/tests/sample.test.mjs
+++ esm/tests/sample.test.mjs
@@ -1,32 +1,32 @@
 import { sum as staticMjsSum } from '../src/sample.mjs';
 import { sum as staticCjsSum } from '../src/sample.cjs';
 
-// ESM style test
-test('adds 1 + 2 to equal 3 (ESM)', () => {
+// ESM style test using static import for .mjs
+test('adds 1 + 2 to equal 3 (static import of mjs)', () => {
   const sum = staticMjsSum;
   expect(sum(1, 2)).toBe(3);
 });
 
-// CommonJS style test
-test('adds 1 + 2 to equal 3 (ESM)', () => {
+// CommonJS style test using static import for .cjs
+test('adds 1 + 2 to equal 3 (static import of cjs)', () => {
   const sum = staticCjsSum;
   expect(sum(1, 2)).toBe(3);
 });
 
-// CommonJS style test using dynamic import
-test('adds 1 + 2 to equal 3 (CommonJS)', async () => {
-  const { sum: cjsSum } = await import('../src/sample.cjs');
-  expect(cjsSum(1, 2)).toBe(3);
-});
-
 // ESM style test using dynamic import for .mjs
-test('adds 5 + 6 to equal 11 (dynamic ESM import)', async () => {
+test('adds 5 + 6 to equal 11 (dynamic import of mjs)', async () => {
   const { sum: dynMjsSum } = await import('../src/sample.mjs');
   expect(dynMjsSum(5, 6)).toBe(11);
 });
 
-// CommonJS style test using dynamic require
-test('adds 3 + 4 to equal 7 (dynamic require)', async () => {
+// CommonJS style test using dynamic import for .cjs
+test('adds 1 + 2 to equal 3 (dynamic import of cjs)', async () => {
+  const { sum: cjsSum } = await import('../src/sample.cjs');
+  expect(cjsSum(1, 2)).toBe(3);
+});
+
+// CommonJS style test using dynamic require for .cjs
+test('adds 3 + 4 to equal 7 (dynamic require of cjs)', async () => {
   const { createRequire } = await import('module');
   const { fileURLToPath } = await import('url');
   const __filename = fileURLToPath(import.meta.url);
@@ -44,5 +44,5 @@ test('throws error when requiring .mjs file using require', async () => {
 
   expect(() => {
     require('../src/sample.mjs');
-  }).toThrow(/must use import to load ES Module/i);
+  }).toThrow(/must use import to load es module/i);
 });
