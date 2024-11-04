bb6c9d2b9b12fe90787db4b1648af4d699c8fbe1
Konstantin Diachenko <drakonard@gmail.com>
2024-11-04 23:18:20 +0530

    Add init-jest.sh script

---
+++ js-jest-repo/init-jest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install Jest and Babel dependencies
+yarn add --dev jest @babel/preset-env babel-jest
+
+# Create .babelrc file
+echo '{
+  "presets": ["@babel/preset-env"]
+}' > .babelrc
+
+# Create jest.config.js file
+echo 'export default {
+  transform: {
+    "^.+\\.cjs$": "babel-jest",
+    "^.+\\.mjs$": "babel-jest"
+  },
+  testMatch: ["**/tests/**/*.[mc][jt]s?(x)", "**/?(*.)+(spec|test).[mc][jt]s?(x)"]
+};' > jest.config.js
+
+# Update package.json using jq
+if [ -f "package.json" ]; then
+  # Add "test" script and set "type" to "module"
+  jq '.scripts.test = "jest" | .type = "module"' package.json > temp.json && mv temp.json package.json
+  echo "Test script and type module set in package.json."
+else
+  echo "package.json not found."
+fi
+
+echo "Setup complete! Jest and Babel have been configured."
